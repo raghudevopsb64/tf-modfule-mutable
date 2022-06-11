832078f36fcdb6c620eb2c87e889bbe1dca028d3
Raghu K <raghuk.vit@gmail.com>
2022-06-11 07:45:07 +0530

    Update roboshop-infra/all-modules/tf-modfule-mutable/

--- ec2.tf
+++ ec2.tf
@@ -5,6 +5,7 @@ resource "aws_spot_instance_request" "spot" {
   vpc_security_group_ids = [aws_security_group.main.id]
   subnet_id              = var.SUBNET_IDS[0]
   wait_for_fulfillment   = true
+  iam_instance_profile   = var.IAM_POLICY_CREATE ? aws_iam_instance_profile.instance-profile.*.name[0] : null
 
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}"
@@ -24,6 +25,7 @@ resource "aws_instance" "on-demand" {
   instance_type          = var.NODE_TYPE
   vpc_security_group_ids = [aws_security_group.main.id]
   subnet_id              = var.SUBNET_IDS[0]
+  iam_instance_profile   = var.IAM_POLICY_CREATE ? aws_iam_instance_profile.instance-profile.*.name[0] : null
 
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}"
