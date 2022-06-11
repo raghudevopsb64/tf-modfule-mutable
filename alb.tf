4b1eab37675e9356171059424f186f9249ec8714
Raghu K <raghuk.vit@gmail.com>
2022-06-11 08:18:56 +0530

    Update roboshop-infra/all-modules/tf-modfule-mutable/

--- alb.tf
+++ alb.tf
@@ -14,3 +14,9 @@ resource "aws_lb_target_group" "target-group" {
   }
 }
 
+resource "aws_lb_target_group_attachment" "instances-to-tg" {
+  count            = length(local.ALL_INSTANCE_IDS)
+  target_group_arn = aws_lb_target_group.target-group.arn
+  target_id        = element(local.ALL_INSTANCE_IDS, count.index)
+  port             = var.PORT
+}
--- locals.tf
+++ locals.tf
@@ -1,3 +1,4 @@
 locals {
   ALL_INSTANCE_IPS = concat(aws_spot_instance_request.spot.*.private_ip, aws_instance.on-demand.*.private_ip)
+  ALL_INSTANCE_IDS = concat(aws_spot_instance_request.spot.*.spot_instance_id, aws_instance.on-demand.*.id)
 }
