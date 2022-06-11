0bf283a09e7087269e0fc8fadbbb5441d6bb8f3e
Raghu K <raghuk.vit@gmail.com>
2022-06-11 08:29:12 +0530

    Update roboshop-infra/all-modules/tf-modfule-mutable/

--- alb.tf
+++ alb.tf
@@ -33,3 +33,15 @@ resource "aws_lb" "alb" {
     ENV  = var.ENV
   }
 }
+
+resource "aws_lb_listener" "component-http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target-group.arn
+  }
+}
+
