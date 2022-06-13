5178ffc0784150a1ccc19ae5a7d805272336f70f
Raghu K <raghuk.vit@gmail.com>
2022-06-13 09:05:27 +0530

    Update roboshop-infra/all-modules/tf-modfule-mutable/

--- app-deploy.tf
+++ app-deploy.tf
@@ -1,18 +1,18 @@
-resource "null_resource" "app-deploy" {
-  count = length(local.ALL_INSTANCE_IPS)
-  provisioner "remote-exec" {
-
-    connection {
-      type     = "ssh"
-      user     = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_USERNAME"]
-      password = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_PASSWORD"]
-      host     = element(local.ALL_INSTANCE_IPS, count.index)
-    }
-
-    inline = [
-      "ansible-pull -U https://github.com/raghudevopsb64/roboshop-ansible.git roboshop.yml -e HOST=localhost -e ROLE_NAME=${var.COMPONENT} -e ENV=${var.ENV}"
-    ]
-  }
-}
-
-
+//resource "null_resource" "app-deploy" {
+//  count = length(local.ALL_INSTANCE_IPS)
+//  provisioner "remote-exec" {
+//
+//    connection {
+//      type     = "ssh"
+//      user     = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_USERNAME"]
+//      password = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_PASSWORD"]
+//      host     = element(local.ALL_INSTANCE_IPS, count.index)
+//    }
+//
+//    inline = [
+//      "ansible-pull -U https://github.com/raghudevopsb64/roboshop-ansible.git roboshop.yml -e HOST=localhost -e ROLE_NAME=${var.COMPONENT} -e ENV=${var.ENV}"
+//    ]
+//  }
+//}
+//
+//
