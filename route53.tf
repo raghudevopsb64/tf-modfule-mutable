b092df48f1005101db5609983eb279134a4e8ba2
Raghu K <raghuk.vit@gmail.com>
2022-06-13 09:07:05 +0530

    Update roboshop-infra/all-modules/tf-modfule-mutable/

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "alb" {
   zone_id = "Z07578712H75FS9NNU2HC"
-  name    = "${var.COMPONENT}-${var.ENV}"
+  name    = "${var.COMPONENT}-${var.ENV}.roboshop.internal"
   type    = "CNAME"
   ttl     = "300"
   records = [aws_lb.alb.dns_name]
