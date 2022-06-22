501956d3e7539e40b389b54073e4429cc2d6fc9d
Raghu K <raghuk.vit@gmail.com>
2022-06-22 07:59:41 +0530

    Update roboshop-infra/all-modules/tf-modfule-mutable/

--- sg.tf
+++ sg.tf
@@ -20,9 +20,9 @@ resource "aws_security_group" "main" {
   }
 
   ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
+    description = "PROMETHEUS"
+    from_port   = 9100
+    to_port     = 9100
     protocol    = "tcp"
     cidr_blocks = [var.PROMETHEUS_IP]
   }
